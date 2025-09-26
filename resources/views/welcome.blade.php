eb39a49cdc1cb746ee5543d8faa80a4dc078a1eb
Dev96xp <dip.seniors@gmail.com>
2025-09-25 22:36:32 -0500

    business.update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -154,7 +154,8 @@
         </h1>
 
         <div
-            class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
+                    {{-- max-w-7xl --}}
+            class="w-full mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-6 gap-y-8">
             {{-- ARTICULO 1 --}}
             <article>
                 <figure>
