47bc5f134e2f1e0ac7b42dcd7bd736df93d5e014
Dev96xp <dip.seniors@gmail.com>
2025-10-13 01:19:24 -0500

    last new vertion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -214,7 +214,7 @@
     {{-- SECCTION 5 - LargeImage A --}}
     <div style="{{ $active5 == 'ACTIVE' ? '' : 'display:none' }}">
 
-        <section class="mt-16 opacity-100 relative bg-fixed bg-size-[100px] lg:bg-cover bg-center bg-no-repeat hover:opacity-100"
+        <section class="w-auto mt-16 opacity-100 relative bg-fixed lg:bg-cover bg-center bg-no-repeat hover:opacity-100"
             style="background-image: url('{{ Storage::url($largeImageA) }}')">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-48">
                 <div class="w-full md:w-3/4 lg:w-1/2">
